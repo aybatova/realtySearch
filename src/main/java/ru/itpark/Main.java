47beaa57fe6e3da08cb8b3f94905e94a4eb3f927
student <user.email>
2018-12-06 10:14:09 +0300

    changed id in house

--- src/main/java/ru/itpark/Main.java
+++ src/main/java/ru/itpark/Main.java
@@ -19,7 +19,7 @@ public class Main {
         service.add(new House(77894, 15, 140, "Звениговский"));
         service.add(new House(15645, 45, 360, "Сармановский"));
         service.add(new House(16487, 28, 280, "Ленинский"));
-        service.add(new House(02165, 10, 70, "Сармановский"));
+        service.add(new House(52165, 10, 70, "Сармановский"));
 
         System.out.println(service.getSorted(new AscPriceComparator()));
         System.out.println(service.getSorted(new DescPriceComparator()));
